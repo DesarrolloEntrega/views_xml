8486a54f16a90395dd627c456a2acac10642ef9a
Marco Montoya Gonzalez <desarrollo1@en-trega.com>
2024-10-04 12:10:14 -0600

    add publishing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,24 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+                groupId = "com.github.DesarrolloEntrega"
+                artifactId = "snackbar"
+                version = "1.0.3"
+            }
+        }
     }
 }
 
